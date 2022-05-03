f3cf4fe2f17ab1ca35c8e87f902286e54d2b4309
AliAmorim <alineamorimxd@hotmail.com>
2022-05-03 19:00:14 -0300

    mudança de configuração

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { routers } from './src/routers/routers.js'
+import path from 'path'
+
+const app = express()
+const port = 3001
+let __dirname = path.resolve(path.dirname(''))
+
+app.set('view engine', 'ejs')
+app.use(routers)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => {
+  console.log(`rodando na porta ${port}`)
+})
