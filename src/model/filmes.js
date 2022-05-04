a61c008bf4328916acf1363622057b91ddeca6a9
AliAmorim <alineamorimxd@hotmail.com>
2022-05-03 21:44:41 -0300

    att

--- src/model/filmes.js
+++ src/model/filmes.js
@@ -1,12 +1,46 @@
+import Sequelize from 'sequelize'
+import { connection } from '../database/connection.js'
+
+export const filmes = connection.define('filmes', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  sinopse: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  diretor: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  img: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  duracao: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  ano: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  iframe: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+}, {
+  freezeTableName: true,
+  createdAT: false,
+  updatedAT: false,
+  timestamps: false
+})
 
 
-{
-  id,
-  nome,
-  sinopse,
-  diretor,
-  img,
-  duracao,
-  ano,
-  iframe
-}
