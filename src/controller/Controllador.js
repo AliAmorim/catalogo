06a56fc520bbba32ab281a835e1483dc0ce5cfc2
AliAmorim <alineamorimxd@hotmail.com>
2022-05-08 18:51:43 -0300

    att

--- src/controller/Controllador.js
+++ src/controller/Controllador.js
@@ -1,3 +1,20 @@
-export const getIndex = (req, res) => {
-  res.render('index.ejs')
+import { connection } from "../database/connection.js"
+import { filmes } from "../model/filmes.js"
+
+
+export const getIndex = async (req, res) => {
+  try {
+  //const listFilmes = await connection.query('SELECT * FROM filmes', {
+    model: filmes
+ // })
+  const listFilmes = await filmes.findAll()
+
+  console.log(listFilmes)
+  res.render('index.ejs', {
+    listFilmes
+  })
+} catch(error) {
+  res.send (error.message)
+}
+
 }
