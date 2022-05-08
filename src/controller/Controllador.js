b2ff5378d57ca5be7748d67d683fb4b3c46184b5
AliAmorim <alineamorimxd@hotmail.com>
2022-05-08 19:38:08 -0300

    att

--- src/controller/Controllador.js
+++ src/controller/Controllador.js
@@ -1,20 +1,29 @@
-import { connection } from "../database/connection.js"
-import { filmes } from "../model/filmes.js"
-
+import { connection } from '../database/connection.js'
+import { filmes } from '../model/filmes.js'
 
 export const getIndex = async (req, res) => {
   try {
-  //const listFilmes = await connection.query('SELECT * FROM filmes', {
+    //const listFilmes = await connection.query('SELECT * FROM filmes', {
     model: filmes
- // })
-  const listFilmes = await filmes.findAll()
+    // })
+    const listFilmes = await filmes.findAll()
 
-  console.log(listFilmes)
-  res.render('index.ejs', {
-    listFilmes
-  })
-} catch(error) {
-  res.send (error.message)
+    console.log(listFilmes)
+    res.render('index.ejs', {
+      listFilmes
+    })
+  } catch (error) {
+    res.send(error.message)
+  }
 }
 
+export const getDetalhes = async (req, res) => {
+  try {
+    const filmesDetalhes = await filmes.findByPk(req.params.id)
+    res.render('detalhes.ejs', {
+      filmesDetalhes
+    })
+  } catch (error) {
+    res.send(error.message)
+  }
 }
