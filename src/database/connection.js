db1b68513a4b4dca5e97c77a830c0d5c7c8f0a66
AliAmorim <alineamorimxd@hotmail.com>
2022-05-09 22:52:11 -0300

    att

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,4 @@
-import  Sequelize  from 'sequelize'
-import dotenv from 'dotenv'
-
-dotenv.config()
+const Sequelize = require("sequelize")
 
 export const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
